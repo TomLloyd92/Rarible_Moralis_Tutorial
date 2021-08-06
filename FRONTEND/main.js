0251814f577a4ec093401f6d938920451d3b85e6
TomLloyd92 <c00231308@itcarlow.ie>
2021-08-06 14:01:21 +0100

    User Profile Saving to Moralis

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -38,10 +38,53 @@ logout = async () =>{
 openUserInfo = async () => {
     user = await Moralis.User.current();
     if(user){
+        //User Email
+        const email = user.get('email')
+        if(email)
+        {
+            userEmailField.value = email;
+        }
+        else
+        {
+            userEmailField.value = "";
+        }
+
+        //User Name
+        userUsernameField.value = user.get('username');
+
+        //Avatar
+        const userAvatar = user.get('avatar')
+        if(userAvatar)
+        {
+            userAvatarImg.src = userAvatar.url();
+        }
+        else
+        {
+            hideElement(userAvatarImg);
+        }
+
         showElement(userInfo);
     }else{
         login();
     }
+}
+
+//Store all user settings
+saveUserInfo = async () =>
+{
+    user.set('email', userEmailField.value);
+    user.set('username', userUsernameField.value);
+
+    if (userAvatarFile.files.length > 0) 
+    {      
+        const avatar = new Moralis.File("avatar.jpeg", userAvatarFile.files[0]);
+        user.set('avatar', avatar);
+    }
+
+    //Save and display
+    await user.save();
+    alert("User Info saved Successfully");
+    openUserInfo();
 }
 
 hideElement = (element) => element.style.display = "none";
@@ -58,8 +101,17 @@ userProfileButton.onclick = openUserInfo;
 
 //Close User Info
 const userInfo = document.getElementById("userInfo");
+const userUsernameField = document.getElementById("txtUsername");
+const userEmailField = document.getElementById("txtEmail");
+const userAvatarImg = document.getElementById("imgAvatar");
+const userAvatarFile = document.getElementById("fileAvatar");
+
 document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo);
+
+//Log out
 document.getElementById("btnLogout").onclick = logout;
+
+document.getElementById("btnSaveUserInfo").onclick = saveUserInfo;
 
 
 init();
